0f4cd582901710d63d51ed57b3a09ae23a316e09
Ahmad Gayibov <ahmad.gayibov@gmail.com>
2024-12-14 13:03:38 +0400

    use terraform latest version

--- terraform/main.tf
+++ terraform/main.tf
@@ -140,7 +140,6 @@ resource "aws_s3_bucket" "nginx_config_bucket" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  provider = aws
   bucket = "my-terraform-state-bucket"
   acl    = "private"
 }
